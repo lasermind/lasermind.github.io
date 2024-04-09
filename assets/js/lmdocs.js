c2db3baa8564902099e7a747b44eac2a1eb7eefa
lasermind <126513496+lasermind@users.noreply.github.com>
2024-04-09 22:28:07 +0200

    Tech-O-Meter modal frame

--- assets/js/lmdocs.js
+++ assets/js/lmdocs.js
@@ -2,3 +2,14 @@
 /* Custom JS for Lasermind's Documents */
 
 console.log("Study Bitcoin");
+
+
+function openTechOMeterModal() {
+  console.log("Open");
+  document.body.classList.add('tech-o-meter-modal-show');
+}
+
+function closeTechOMeterModal() {
+  console.log("Close");
+  document.body.classList.remove('tech-o-meter-modal-show');
+}
